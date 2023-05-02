4342dd09216b40e5682b4a58924188492e3ef055
nathaniel <na.diamond@simpsgroup.co.uk>
2023-05-02 09:03:43 +0100

    add login svgs

--- lendsqr-test/src/pages/Auth.tsx
+++ lendsqr-test/src/pages/Auth.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import "./App.scss";
+// import lendsqr from "../../public/images/lendsqr.svg";
+// import logo from "../../public/images/Union.png";
+// import artWork from "./images/onboardingImg.svg";
+
 // import { useNavigate } from "react-router-dom";
+import Lendsqr from "../assets/images/lendsqr.svg";
 
 function Auth() {
   const [email, setEmail] = useState("");
@@ -25,29 +30,42 @@ function Auth() {
   };
 
   return (
-    <div>
-      <h1>Authentication Page</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <br />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+    <div className="authPage">
+      <div className="leftAuthPage">
+        <span className="logo">
+          {/* <img src={logo} alt="logo" className="iconLogo" /> */}
+
+          <img src={Lendsqr} alt="text" />
+        </span>
+        <span className="onboarding">
+          {/* <img src={artWork} alt="artwork" /> */}
+        </span>
+      </div>
+
+      <div className="rightAuthPage">
+        <h1>Authentication Page</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <br />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
