dba653ba7b7607365565ba024173576b23a3ab5d
DiamondNath07 <diamondnathaniel2@gmail.com>
2023-05-05 19:48:22 +0100

    add usertypes to dashboard

--- lendsqr-test/src/pages/dashboard/Dashboard.tsx
+++ lendsqr-test/src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React  from 'react';
+import { userTypes } from './dashboardData';
+import { Fragment } from 'react';
 
 export default function Dashboard() {
   return (
-    <div>
-      <p>Dashboard</p>
+    <div className="userDashboard">
+      <section className="userTypes">
+        <h2>users</h2>
+        <div className="userCards">
+          {userTypes.map((userType) => (
+            <Fragment key={userType.id}>
+
+              <div >{userType.description}</div>
+            </Fragment>
+          ))}
+          
+        </div>
+      </section>
+      <section></section>
     </div>
   );
 }
---
+++ lendsqr-test/src/pages/dashboard/dashboardData.ts
@@ -0,0 +1,32 @@
+export const userTypes = [
+  {
+    id: 1,
+    description: 'usertypes',
+    children: [
+      {
+        id: 1.1,
+        icon: 'entity icon',
+        item: 'users',
+        count: 2453,
+      },
+      {
+        id: 1.2,
+        icon: 'entity icon',
+        item: 'active users',
+        count: 2453,
+      },
+      {
+        id: 1.3,
+        icon: 'entity icon',
+        item: 'users with loans',
+        count: 2453,
+      },
+      {
+        id: 1.4,
+        icon: 'entity icon',
+        item: 'users with savings',
+        count: 102453,
+      },
+    ],
+  },
+];
