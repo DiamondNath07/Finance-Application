28ec7d521b240bd3ed14a3b31d4f151be7268be6
nathaniel <na.diamond@simpsgroup.co.uk>
2023-05-07 00:19:33 +0100

    add changes yto status column

--- lendsqr-test/src/pages/dashboard/Tbody.tsx
+++ lendsqr-test/src/pages/dashboard/Tbody.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { IUserData } from '../../components/interface';
-import '../../pages/App.scss';
-import elipsis from '../../assets/images/elipsis.png';
-import moment from 'moment';
+import React from "react";
+import { IUserData } from "../../components/interface";
+import "../../pages/App.scss";
+import elipsis from "../../assets/images/elipsis.png";
+import moment from "moment";
 
 export default function Tbody({ user }: { user: IUserData }) {
   return (
     <tr className="tBody">
-      <td className='orgName'>{user.orgName}</td>
+      <td className="orgName">{user.orgName}</td>
       <td>{user.userName}</td>
-      <td className='email'>{user.email}</td>
+      <td className="email">{user.email}</td>
       <td>{user.phoneNumber}</td>
-      <td>{moment(user.createdAt).format('MMMM Do YYYY, h:mm:ss A')}</td>
-      <td>{user.education.employmentStatus}</td>
-      <td className='optionBar'>
+      <td>{moment(user.createdAt).format("MMMM Do YYYY, h:mm:ss A")}</td>
+      <td className="status">{user.education.employmentStatus}</td>
+      <td className="optionBar">
         <img src={elipsis} alt="" />
       </td>
     </tr>
