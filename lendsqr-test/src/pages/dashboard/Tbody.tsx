54b2befdf7f5e75abe70015fa46073bb1d175a6d
DiamondNath07 <diamondnathaniel2@gmail.com>
2023-05-06 12:18:02 +0100

    add style to userdashboard

--- lendsqr-test/src/pages/dashboard/Tbody.tsx
+++ lendsqr-test/src/pages/dashboard/Tbody.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { IUserData } from "../../components/interface";
-import moment from "moment";
+import React from 'react';
+import { IUserData } from '../../components/interface';
+import '../../pages/App.scss';
+import elipsis from '../../assets/images/elipsis.png';
+import moment from 'moment';
 
 export default function Tbody({ user }: { user: IUserData }) {
   return (
-    <tr>
+    <tr className="tBody">
       <td>{user.orgName}</td>
       <td>{user.userName}</td>
       <td>{user.email}</td>
       <td>{user.phoneNumber}</td>
-      <td>{moment(user.createdAt).format("MMMM Do YYYY, h:mm:ss A")}</td>
+      <td>{moment(user.createdAt).format('MMMM Do YYYY, h:mm:ss A')}</td>
       <td>{user.education.employmentStatus}</td>
-      <td>...</td>
+      <td className='optionBar'>
+        <img src={elipsis} alt="" />
+      </td>
     </tr>
   );
 }
--- lendsqr-test/src/pages/dashboard/Thead.tsx
+++ lendsqr-test/src/pages/dashboard/Thead.tsx
@@ -1,15 +1,28 @@
-import React from "react";
-
+import React from 'react';
+import filter from '../../assets/images/filter.png';
+import '../../pages/App.scss';
 export default function Thead() {
   return (
     <>
-      <tr>
-        <th>ORGANIZATION</th>
-        <th>USERNAME</th>
-        <th>EMAIL</th>
-        <th>PHONE NUMBER</th>
-        <th>DATE JOINED</th>
-        <th>STATUS</th>
+      <tr className="tableRow">
+        <th>
+          ORGANIZATION <img src={filter} alt="filterIcon" />
+        </th>
+        <th>
+          USERNAME <img src={filter} alt="filterIcon" />
+        </th>
+        <th>
+          EMAIL <img src={filter} alt="filterIcon" />
+        </th>
+        <th>
+          PHONE NUMBER <img src={filter} alt="filterIcon" />
+        </th>
+        <th>
+          DATE JOINED <img src={filter} alt="filterIcon" />
+        </th>
+        <th>
+          STATUS <img src={filter} alt="filterIcon" />
+        </th>
         <th></th>
       </tr>
     </>
